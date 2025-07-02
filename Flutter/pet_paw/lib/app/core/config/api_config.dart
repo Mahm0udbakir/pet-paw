cdaf5c9f861ea751eea1a553224f46865476c174
Omama Saour <omama.basher.saour@gmail.com>
2025-07-02 17:23:45 +0300

    Added reset password files

--- Flutter/pet_paw/lib/app/core/config/api_config.dart
+++ Flutter/pet_paw/lib/app/core/config/api_config.dart
@@ -4,4 +4,9 @@ class ApiConfig {
   // Endpoints
   static const String login = '/api/Account/Login';
   static const String signup = '/api/Account/signup';
+
+  // reset password
+  static const String forgetPassword = '/api/Account/ForgetPassword';
+  static const String verifyOTP = '/api/Account/VerifyOTP';
+  static const String resetPassword = '/api/Account/ResetPassword';
 }
