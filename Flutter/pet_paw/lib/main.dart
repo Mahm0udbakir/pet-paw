f12bfbc87c4bbbbf686f6de4c928cf4b69023b17
Salah Fathy <SalahFathy@gmail.com>
2025-07-07 20:43:45 +0300

    some updates

--- Flutter/pet_paw/lib/main.dart
+++ Flutter/pet_paw/lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => const PetPaw(),
+      builder: (context, child) => SafeArea(child: const PetPaw()),
     ),
   );
 }
